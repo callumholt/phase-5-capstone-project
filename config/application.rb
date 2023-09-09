cb6a18da2825e5ef5103c82cf3223aa4d4616d03
callum <callumpholt@gmail.com>
2023-09-10 07:08:01 +1000

    changed from third party software to openai api

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,6 @@ module ReactRailsApiProjectTemplate
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    require 'dotenv/load'
 
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,6 +111,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  require 'dotenv/load'
 
 end
