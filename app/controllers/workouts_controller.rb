ebeb4f4b710beecc24b08b7c694a79c54deb8084
callum <callumpholt@gmail.com>
2023-09-10 19:40:56 +1000

    updates to openai fetch

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -52,8 +52,14 @@ def openAiCall
   
   # Define the message parameters
   messages = [
-    { role: 'system', content: 'You are a helpful assistant.' },
-    { role: 'user', content: 'can you make me a list of 3 country names' }
+    {
+      "role": "system",
+      "content": "Generate JSON data for a workout plan for a user with the details provided by the user.\n\nall exercises must have reps and weight.\n\nReturn the JSON data using the structure below as an example:\n\nPlease ensure you use different exercises than those in the example.\n\n{{\"user_id\": 4,\"name\":\"User: Bruce - Workout\",\"day\":[{\"id\":1,\"dayNumber\":1,\"exercises\":[{\"id\":1,\"name\":\"Bench Press\",\"prescribed_sets\":[{\"id\":1,\"weight\":\"80.0\",\"reps\":3},{\"id\":2,\"weight\":\"85.0\",\"reps\":3},{\"id\":3,\"weight\":\"90.0\",\"reps\":3}]},{\"id\":2,\"name\":\"Deadlift\",\"prescribed_sets\":[{\"id\":4,\"weight\":\"100.0\",\"reps\":5},{\"id\":5,\"weight\":\"105.0\",\"reps\":5}]}]},{\"id\":2,\"dayNumber\":2,\"exercises\":[{\"id\":3,\"name\":\"Back Squats\",\"prescribed_sets\":[{\"id\":6,\"weight\":\"80.0\",\"reps\":3},{\"id\":7,\"weight\":\"85.0\",\"reps\":3},{\"id\":8,\"weight\":\"90.0\",\"reps\":3}]},{\"id\":4,\"name\":\"Pull-Ups\",\"prescribed_sets\":[{\"id\":9,\"weight\":\"100.0\",\"reps\":5},{\"id\":10,\"weight\":\"105.0\",\"reps\":5}]}]},{\"id\":3,\"dayNumber\":3,\"exercises\":[{\"id\":5,\"name\":\"Push Press\",\"prescribed_sets\":[{\"id\":11,\"weight\":\"80.0\",\"reps\":3},{\"id\":12,\"weight\":\"85.0\",\"reps\":3},{\"id\":13,\"weight\":\"90.0\",\"reps\":3}]},{\"id\":6,\"name\":\"Barbell Rows\",\"prescribed_sets\":[{\"id\":14,\"weight\":\"100.0\",\"reps\":5},{\"id\":15,\"weight\":\"105.0\",\"reps\":5}]}]},{\"id\":4,\"dayNumber\":4,\"exercises\":[{\"id\":7,\"name\":\"Leg Press\",\"prescribed_sets\":[{\"id\":16,\"weight\":\"80.0\",\"reps\":3},{\"id\":17,\"weight\":\"85.0\",\"reps\":3},{\"id\":18,\"weight\":\"90.0\",\"reps\":3}]},{\"id\":8,\"name\":\"Chest Flyes\",\"prescribed_sets\":[{\"id\":19,\"weight\":\"100.0\",\"reps\":5},{\"id\":20,\"weight\":\"105.0\",\"reps\":5}]}]},{\"id\":5,\"dayNumber\":5,\"exercises\":[{\"id\":9,\"name\":\"Dumbbell Curls\",\"prescribed_sets\":[{\"id\":21,\"weight\":\"80.0\",\"reps\":3},{\"id\":22,\"weight\":\"85.0\",\"reps\":3},{\"id\":23,\"weight\":\"90.0\",\"reps\":3}]},{\"id\":10,\"name\":\"Tricep Dips\",\"prescribed_sets\":[{\"id\":24,\"weight\":\"100.0\",\"reps\":5},{\"id\":25,\"weight\":\"105.0\",\"reps\":5}]}]}]}}"
+    },
+    {
+      "role": "user",
+      "content": "Please create a workout designed for the user below:\n\nname: Bruce\nuser_id: 4\nFitness goals: gain muscle\nTraining experience: intermediate\nDays per week to train: 5\nTime for each training session: 60 minutes\nInjuries: tight hips\n"
+    }
   ]
   
   
@@ -61,7 +67,13 @@ def openAiCall
   request = Net::HTTP::Post.new(url)
   request.body = {
     model: 'gpt-3.5-turbo',
-    messages: messages
+    messages: messages,
+    temperature: 1,
+    temperature: 1,
+    max_tokens: 500,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
   }.to_json
   request.initialize_http_header(headers)
   
